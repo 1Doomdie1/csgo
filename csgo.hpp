c3536e18053620b2f1005ee7cb8b26b1e159e588
1Doomdie1 <horia.todoran@yahoo.com>
2022-01-26 19:08:43 +0000

    26.01.2022

--- csgo.hpp
+++ csgo.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <cstdint>
 
-// 2022-01-26 00:26:50.636647300 UTC
+// 2022-01-26 19:08:22.725399 UTC
 
 namespace hazedumper {
-constexpr ::std::int64_t timestamp = 1643156810;
+constexpr ::std::int64_t timestamp = 1643224102;
 namespace netvars {
 constexpr ::std::ptrdiff_t cs_gamerules_data = 0x0;
 constexpr ::std::ptrdiff_t m_ArmorValue = 0x117CC;
@@ -146,7 +146,7 @@ constexpr ::std::ptrdiff_t dwYawPtr = 0xDBBEE8;
 constexpr ::std::ptrdiff_t dwZoomSensitivityRatioPtr = 0xDC1D38;
 constexpr ::std::ptrdiff_t dwbSendPackets = 0xD93D2;
 constexpr ::std::ptrdiff_t dwppDirect3DDevice9 = 0xA5050;
-constexpr ::std::ptrdiff_t find_hud_element = 0x27E54760;
+constexpr ::std::ptrdiff_t find_hud_element = 0x28894760;
 constexpr ::std::ptrdiff_t force_update_spectator_glow = 0x3B88AA;
 constexpr ::std::ptrdiff_t interface_engine_cvar = 0x3E9EC;
 constexpr ::std::ptrdiff_t is_c4_owner = 0x3C5890;
