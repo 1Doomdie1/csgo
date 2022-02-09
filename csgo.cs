36189220bab923ba42701625c4ba1d1fdf8e5524
1Doomdie1 <horia.todoran@yahoo.com>
2022-02-09 09:39:56 +0000

    09.02.2022

--- csgo.cs
+++ csgo.cs
@@ -1,10 +1,10 @@
 using System;
 
-// 2022-02-05 02:28:48.276426800 UTC
+// 2022-02-09 09:39:32.283165500 UTC
 
 namespace hazedumper
 {
-    public const Int32 timestamp = 1644028128;
+    public const Int32 timestamp = 1644399572;
     public static class netvars
     {
         public const Int32 cs_gamerules_data = 0x0;
@@ -148,7 +148,7 @@ namespace hazedumper
         public const Int32 dwZoomSensitivityRatioPtr = 0xDBEF60;
         public const Int32 dwbSendPackets = 0xD93D2;
         public const Int32 dwppDirect3DDevice9 = 0xA5050;
-        public const Int32 find_hud_element = 0x27DD48A0;
+        public const Int32 find_hud_element = 0x286248A0;
         public const Int32 force_update_spectator_glow = 0x3BA6CA;
         public const Int32 interface_engine_cvar = 0x3E9EC;
         public const Int32 is_c4_owner = 0x3C76A0;
